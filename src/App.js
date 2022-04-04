9896f304160cefb9fff5d8c7e0e01fd05ea94e83
Banstein <islambahnas1@gmail.com>
2022-04-04 16:16:52 +0200

    Setup Basics and Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Navbar from './componnent/Navbar';
 
 function App() {
   return (
-    <div>Hi!</div>
+    <Navbar />
   );
 }
 
