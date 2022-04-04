5d353c187b99a07df3d8dde40718a9fb0b766ca9
Banstein <islambahnas1@gmail.com>
2022-04-04 14:50:30 +0200

    setup template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div>Hi!</div>
+  );
+}
+
+export default App;
