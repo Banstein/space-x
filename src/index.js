c630ede9261c8b035ee38e7b9e943bc9524e5cb1
Banstein <islambahnas1@gmail.com>
2022-04-06 01:13:04 +0200

    Add the store in the application

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './CSS/index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/missions/configureStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
