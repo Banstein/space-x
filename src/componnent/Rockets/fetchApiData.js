dce9ce4c7f6f471fb978494716a04d1d46ca12d7
Cynthia Inga <candyinga67@gmail.com>
2022-04-05 17:22:32 +0200

    Make changes

--- src/componnent/Rockets/fetchApiData.js
+++ src/componnent/Rockets/fetchApiData.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
+
 const ROCKETS_API = 'https://api.spacexdata.com/v3/rockets)';
 
 const fetchRocketsApiData = async () => {
-  const getData = await fetch.get(ROCKETS_API);
+  const getData = await axios.get(ROCKETS_API);
   const Rockets = [];
 
   getData.data.forEach((rocketItem) => {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './CSS/index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
