4817c268690244620144270f62e1bce42f8170da
Cynthia Inga <candyinga67@gmail.com>
2022-04-06 10:47:10 +0200

    Display the rockets list

--- src/componnent/Rockets/Rockets.js
+++ src/componnent/Rockets/Rockets.js
@@ -1,18 +1,43 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { rocketDispatcher } from '../../redux/rockets/rockets';
+import RocketCard from './RocketCard';
 
 const Rockets = () => {
   const dispatch = useDispatch();
+  const rocketsData = useSelector((state) => state.rocketsReducer);
 
   useEffect(() => {
-    dispatch(rocketDispatcher());
+    if (!rocketsData.isDataStored) {
+      dispatch(rocketDispatcher());
+    }
   }, []);
 
+  const rockets = rocketsData.data;
+  let rocketsList = [];
+  if (rockets) {
+    rocketsList = rockets.map((rocket) => (
+      <li key={rocket.id} className="rocket-item">
+        <RocketCard
+          className="rocket-card"
+          description={rocket.description}
+          image={rocket.image}
+          name={rocket.rocket_name}
+          id={rocket.id}
+          reserved={rocket.reserved}
+        />
+      </li>
+    ));
+  }
+
   return (
-    <div>
-      <h1>Rockets Page</h1>
-    </div>
+    <section>
+      <div className="rockets">
+        <ul classeName="rocket-list">
+          {rocketsList}
+        </ul>
+      </div>
+    </section>
   );
 };
 
